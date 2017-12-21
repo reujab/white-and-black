aff91a5065e9410f73d10e14f34befae75a14456
reujab <reujab@gmail.com>
2017-12-20 19:50:18 -0500

    added all decks to settings component

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -8,6 +8,8 @@ import Tooltip from "material-ui/Tooltip"
 import Typography from "material-ui/Typography"
 import {FormGroup, FormControlLabel} from "material-ui/Form"
 
+import deckMetadata from "./deck-metadata"
+
 class Settings extends React.Component {
 	constructor(props) {
 		super(props)
@@ -26,29 +28,7 @@ class Settings extends React.Component {
 	}
 
 	render() {
-		const decks = [
-			["base", "Base", 90, 460],
-			["red", "Red Box", 70, 230],
-			["blue", "Blue Box", 80, 220],
-			["green", "Green Box", 55, 245],
-			["90s", "90s Nostalgia", 7, 23],
-			["box", "Box Expansion", 0, 21],
-			["fantasy", "Fantasy", 6, 26],
-			["food", "Food", 6, 24],
-			["science", "Science", 7, 23],
-			["www", "World Wide Web", 9, 21],
-			["hillary", "Hillary", 3, 11],
-			["trump", "Trump", 6, 34],
-			["holiday", "Holiday", 16, 44],
-			["pax", "PAX", 31, 121],
-			["house of cards", "House of Cards", 9, 16],
-			["reject", "Reject", 18, 40],
-			["canadian", "Canadian", 5, 21],
-			["apples2apples", "Apples to Apples\u00ae", 249, 747],
-			["crabs adjust humidity", "Crabs Adjust Humidity", 129, 320],
-			["cads about matrimony", "Cads About Matrimony", 55, 230],
-			// TODO: more decks
-		].map((deck) => <Tooltip key={deck[0]} title={`${deck[2]} Black Cards, ${deck[3]} White Cards`}>
+		const decks = deckMetadata.map((deck) => <Tooltip key={deck[0]} title={`${deck[2]} Black Cards, ${deck[3]} White Cards`}>
 			<FormControlLabel label={deck[1]} control={
 				<Checkbox onChange={(e) => {
 					if (e.target.checked) {
