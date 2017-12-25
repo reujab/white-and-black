7b0ada5c340370ad50799c5b521dfff9e88f8d9f
reujab <reujab@gmail.com>
2017-12-25 17:43:44 -0500

    simplified code

--- src/Game.jsx
+++ src/Game.jsx
@@ -78,10 +78,7 @@ class Game extends React.Component {
 			<Fragment>
 				<Snackbar open={!!this.state.error} message={this.state.error} />
 				<BlankCard show={this.state.fillingBlank} onChange={this.selectCard.bind(this)} />
-				<UsernamePicker
-					username={this.state.username}
-					onChange={this.setUsername.bind(this)}
-				/>
+				<UsernamePicker username={this.state.username} onChange={this.setUsername.bind(this)} />
 
 				<Header username={this.state.username} />
 				<Grid
